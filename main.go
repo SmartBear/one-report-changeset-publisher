0ac4dd0d5519bac733f9fcd13792c586317b544d
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-02-03 12:53:41 +0000

    Add a main

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+  "flag"
+  "github.com/SmartBear/lhdiff"
+  "io/ioutil"
+)
+
+func main() {
+  compact := flag.Bool("compact", false, "Exclude identical lines from output")
+  flag.Parse()
+  leftFile := flag.Arg(0)
+  rightFile := flag.Arg(1)
+  left, _ := ioutil.ReadFile(leftFile)
+  right, _ := ioutil.ReadFile(rightFile)
+  linePairs, leftCount, newRightLines := lhdiff.Lhdiff(string(left), string(right), 4)
+  lhdiff.PrintLinePairs(linePairs, leftCount, newRightLines, !*compact)
+}
