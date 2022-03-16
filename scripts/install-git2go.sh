f9a517e087c808eb5280f5f6a90127399a078bb0
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-03-16 12:01:32 +0000

    Try setting GO111MODULE before we build

--- scripts/install-git2go.sh
+++ scripts/install-git2go.sh
@@ -2,6 +2,7 @@
 export GOPATH=$(go env GOPATH)
 export GO111MODULE=off
 go get github.com/libgit2/git2go/v33
+export GO111MODULE=on
 cd $GOPATH/src/github.com/libgit2/git2go
 git submodule update --init
 make install-static
