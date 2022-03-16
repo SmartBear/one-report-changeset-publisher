6ea49ce703bd801e276441551fe2bda42e1c4335
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-03-16 11:56:19 +0000

    Install dependencies

--- scripts/install-git2go.sh
+++ scripts/install-git2go.sh
@@ -1,14 +1,7 @@
 #!/usr/bin/env bash
 export GOPATH=$(go env GOPATH)
 export GO111MODULE=off
-go get -d github.com/libgit2/git2go/v33
-
-ls -al $GOPATH
-ls -al $GOPATH/src
-ls -al $GOPATH/src/github.com
-ls -al $GOPATH/src/github.com/libgit2
-ls -al $GOPATH/src/github.com/libgit2/git2go
-
+go get github.com/libgit2/git2go/v33
 cd $GOPATH/src/github.com/libgit2/git2go
 git submodule update --init
 make install-static
