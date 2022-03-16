be9b90d454d147a0a19fc1d89b5a356abc1dfbe6
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-03-16 11:54:36 +0000

    Try installing again

--- scripts/install-git2go.sh
+++ scripts/install-git2go.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
-GOPATH=$(go env GOPATH)
+export GOPATH=$(go env GOPATH)
+export GO111MODULE=off
 go get -d github.com/libgit2/git2go/v33
 
 ls -al $GOPATH
@@ -8,6 +9,6 @@ ls -al $GOPATH/src/github.com
 ls -al $GOPATH/src/github.com/libgit2
 ls -al $GOPATH/src/github.com/libgit2/git2go
 
-cd $GOPATH/src/github.com/libgit2/git2go/v33
+cd $GOPATH/src/github.com/libgit2/git2go
 git submodule update --init
 make install-static
