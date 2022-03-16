1904c9b54f19ae76aea7c8f9a4b65e7370e53eb1
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-03-16 11:20:05 +0000

    Moer debug

--- scripts/install-git2go.sh
+++ scripts/install-git2go.sh
@@ -2,6 +2,10 @@
 GOPATH=$(go env GOPATH)
 go get -d github.com/libgit2/git2go/v33
 
+ls -al $GOPATH
+ls -al $GOPATH/src
+ls -al $GOPATH/src/github.com
+ls -al $GOPATH/src/github.com/libgit2
 ls -al $GOPATH/src/github.com/libgit2/git2go
 
 cd $GOPATH/src/github.com/libgit2/git2go/v33
