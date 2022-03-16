ecc0210d72269e08efd245fdeef9441cecb277c2
Aslak Hellesøy <aslak.hellesoy@gmail.com>
2022-03-16 11:15:06 +0000

    Debugging

--- scripts/install-git2go.sh
+++ scripts/install-git2go.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
+GOPATH=$(go env GOPATH)
 go get -d github.com/libgit2/git2go/v33
+
+ls -al $GOPATH/src/github.com/libgit2/git2go
+
 cd $GOPATH/src/github.com/libgit2/git2go/v33
 git submodule update --init
 make install-static
